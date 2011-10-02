878d8f1d132ee15ed1ecd7c1184b52e9cc5101db
Richie Bielak <richieb@richieb-VirtualBox.(none)>
2011-10-01 20:33:03 -0400

    Fixed up indentation of the code

--- book_list.py
+++ book_list.py
@@ -8,80 +8,78 @@ import string
 class Book_List:
 
     def __init__ (self):
-	self.contents = []
+        self.contents = []
     
 
     def make_from_file (self, file):
-	#
-	# Read the file and create a book list
-	#
-	lines = file.readlines ()
-	self.contents = []
-	#
-	# Parse each line and create a list of Book objects
-	#
-	for one_line in lines:
-	    # It's  not a comment or empty line 
-	    if (len(one_line) > 0) and (one_line[0] != "#"):
-		    # Split into tokens
-		    tokens = string.split (one_line)
-		    if len (tokens) > 0:
-			if (tokens[0] == "title:"):
-			    current_book = book.Book (string.join (tokens[1:]))
-			    self.contents.append (current_book)
-			elif (tokens[0] == "author:"):
-			    current_book.set_author (string.join (tokens[1:]))
-			elif (tokens[0] == "subject:"):
-			    current_book.set_subject (string.join (tokens[1:]))
-			elif (tokens[0] == "url:"):
-			    current_book.set_url (string.join (tokens[1:]))
-	
+        #
+        # Read the file and create a book list
+        #
+        lines = file.readlines ()
+        self.contents = []
+        #
+        # Parse each line and create a list of Book objects
+        #
+        for one_line in lines:
+            # It's  not a comment or empty line
+            if (len(one_line) > 0) and (one_line[0] != "#"):
+                # Split into tokens
+                tokens = string.split (one_line)
+                if len (tokens) > 0:
+                    if (tokens[0] == "title:"):
+                        current_book = book.Book (string.join (tokens[1:]))
+                        self.contents.append (current_book)
+                    elif (tokens[0] == "author:"):
+                        current_book.set_author (string.join (tokens[1:]))
+                    elif (tokens[0] == "subject:"):
+                        current_book.set_subject (string.join (tokens[1:]))
+                    elif (tokens[0] == "url:"):
+                        current_book.set_url (string.join (tokens[1:]))
+
 			    
     def sort_by_author (self):
-	#
-	# Sort book list by author
-	#
-	def cmp_author (x, y):
-	    res = cmp (x.last_name, y.last_name)
-	    if res == 0:
-			res = cmp (x.first_name, y.first_name)
-			if res == 0:
-				res = cmp (x.title, y.title)
-	    return res
+        #
+        # Sort book list by author
+        #
+        def cmp_author (x, y):
+            res = cmp (x.last_name, y.last_name)
+            if res == 0:
+                res = cmp (x.first_name, y.first_name)
+                if res == 0:
+                    res = cmp (x.title, y.title)
+            return res
 
-	self.contents.sort (cmp_author)
-	
+        self.contents.sort (cmp_author)
+
 
     def sort_by_title (self):
-	#
-	# Sort book list by title
-	#
-	self.contents.sort (lambda x, y: cmp (x.title, y.title))
-
-
+        #
+        # Sort book list by title
+        #
+        self.contents.sort (lambda x, y: cmp (x.title, y.title))
 
 
     def sort_by_subject (self):
-	#
-	# Sort by subject
-	#
-	def cmp_subject (x, y):
-		res = cmp (x.subject, y.subject)
-		if res == 0:
-			res = cmp (x.title, y.title)
-		return res
+        #
+        # Sort by subject
+        #
+        def cmp_subject (x, y):
+            res = cmp (x.subject, y.subject)
+            if res == 0:
+                res = cmp (x.title, y.title)
+            return res
 
-	self.contents.sort (cmp_subject)
+        self.contents.sort (cmp_subject)
 
 
     def display (self):
-	#
-	# Print the contents of the list
-	#
-	for b in self.contents:
-	    print "-----------------"
-	    b.display ()
-	print "-----------------"
+        #
+        # Print the contents of the list
+        #
+        for b in self.contents:
+            print "-----------------"
+            b.display ()
+        print "-----------------"
 
 #
 # Code to test this class
--- html_page.py
+++ html_page.py
@@ -6,37 +6,37 @@
 class Html_Page:
 
     def __init__ (self, t="", h=""):
-	self.title = t
-	self.heading = h
+        self.title = t
+        self.heading = h
 
     def generate_heading (self, bgcolor=""):
-	#
-	# Generate heading for a page
-	#
-	self.f.write ("<html>\n")
-	self.f.write ("<head>\n")
-	self.f.write ("<title>" + self.title + "</title>\n")
-	self.f.write ("</head>\n")
-	self.f.write ("<body bgcolor=" + bgcolor + ">\n")
-	self.f.write ("<h1 align=center>" + self.heading + "</h1>\n")
+        #
+        # Generate heading for a page
+        #
+        self.f.write ("<html>\n")
+        self.f.write ("<head>\n")
+        self.f.write ("<title>" + self.title + "</title>\n")
+        self.f.write ("</head>\n")
+        self.f.write ("<body bgcolor=" + bgcolor + ">\n")
+        self.f.write ("<h1 align=center>" + self.heading + "</h1>\n")
 
     def generate_body (self):
-	#
-	# Empty function - to be redefined in a descendant
-	#
-	print ""
+        #
+        # Empty function - to be redefined in a descendant
+        #
+        print ""
 
     def generate_trailer (self):
-	#
-	# generate the trailer for a page
-	#
-	self.f.write ("</body>\n")
-	self.f.write ("</html>\n")
+        #
+        # generate the trailer for a page
+        #
+        self.f.write ("</body>\n")
+        self.f.write ("</html>\n")
     
     def generate (self, bgcolor=""):
-	self.generate_heading (bgcolor)
-	self.generate_body ()
-	self.generate_trailer ()
+        self.generate_heading (bgcolor)
+        self.generate_body ()
+        self.generate_trailer ()
 
 #
 # Code to test this class
